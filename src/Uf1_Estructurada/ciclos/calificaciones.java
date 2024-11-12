ec4d83128efb2a4452e3ae1301ef52396b0d1743
gregory2023 <gecanoram@gmail.com>
2024-11-12 01:48:15 +0100

    agrego practicas libres

--- src/Uf1_Estructurada/ciclos/calificaciones.java
+++ src/Uf1_Estructurada/ciclos/calificaciones.java
@@ -1,2 +1,28 @@
-package Uf1_Estructurada.ciclos;public class calificaciones {
+package Uf1_Estructurada.ciclos;
+
+import java.util.Scanner;
+
+public class calificaciones {
+
+    public static void main (String [] args ){
+        Scanner Entrada = new Scanner(System.in);
+        float calificacion ;
+        int suspenso=0;
+        boolean suspensos = false;
+
+        for (int i = 1; i <=10 ; i++) {
+            System.out.println("imgrese la calificacion numero ");
+            calificacion = Entrada.nextInt();
+
+            if (calificacion<5){
+                suspenso++;
+                System.out.println(" supenso numero " + suspenso);
+                suspensos=true;
+
+            }
+        }
+        System.out.println("Hay algun  suspenso " + suspensos);
+        System.out.println("cantidad de suspensos " + suspenso);
+
+    }
 }
--- src/Uf1_Estructurada/ciclos/numeroNegativ.java
+++ src/Uf1_Estructurada/ciclos/numeroNegativ.java
@@ -1,2 +1,29 @@
-package Uf1_Estructurada.ciclos;public class numeroNegativ {
+package Uf1_Estructurada.ciclos;
+
+import java.util.Scanner;
+
+public class numeroNegativ {
+
+    public static void main (String [] args){
+        Scanner Entrada = new Scanner(System.in);
+        int numero ;
+        int numeronNegativo=0;
+        boolean numerosNegativos=false;
+
+        for (int i = 1; i <=10 ; i++) {
+
+
+                System.out.println("ingrese un numero ");
+                numero=Entrada.nextInt();
+
+                if (numero<0) {
+                    numeronNegativo++;
+                    numerosNegativos = true;
+                }
+
+        }
+
+        System.out.println(" hay numeros negativos " + numerosNegativos);
+        System.out.println("hay esta cantidad de numeros negativos " + numeronNegativo );
+    }
 }
